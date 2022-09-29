d4544db266255cf49eb9d6d55a8704c32bc34232
lipo <85422792+thiliporace@users.noreply.github.com>
2022-09-29 16:08:00 -0300

    Add files via upload

--- trabalho.asm
+++ trabalho.asm
@@ -1,69 +1,74 @@
+# Thiago Leandro Liporace - 42128481
+# Kenny Jun Takahashi - 42146143
+# Lucas Goulart de Farias Meres - 42127459
 
 .data
-
-vetor: .word   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
-size: .word   15
-min:  .asciiz "\nElemento Minimo: " 
-max:  .asciiz "\nElemento Maximo: "
-result: .asciiz "\nSoma:"
+max: .asciiz "\nMaior elemento: "
+min: .asciiz "\nMenor elemento: "
+resp: .asciiz "\nMedia dos elementos: "
+vetor: .word 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+size: .word 15
 
 .text
-
-main:   
-    la $a0, vetor
-    lw $a1, size
-    lw $t2, ($a0) # max
-    lw $t3, ($a0) # min
-    lw $t4, ($a0)
-    li $t0,0
-    loop_array:
-        beq $a1, $zero, resp
-        lw $t0, ($a0)
-        bge $t0, $t3, not_min 
-        move $t3, $t0
-        not_min:
-        ble $t0, $t2, not_max 
-        move $t2, $t0
-        not_max:
-        addi $a1, $a1, -1
-        addi $a0, $a0, 4
-        j loop_array
-     loop:
-        beq $a1, $zero,resp
-        lw $s1,0($a0)
-        add $t0, $t0, $s1
-        add $t4,4
-        subi $a1,$a1,1
-        j loop
-        
-    resp:
-    li $v0,4
-    la $a0,result
-    syscall
+main:   la $a0, vetor
+    	lw $a1, size
+    	
+    	li $t2, 0
+    	li $t3, 100
+    	
+    	jal leVetor
+    	li $v0, 10  
+    	syscall 
+    	
+    	
+leVetor:
+    li $t0, 0
+    li $t1, 0
     
-    li $v0,1
-    move $t4,$t0
-    syscall
+loop:    bge $t0, $a1, final 	
+    lw $a0, vetor($t1) 		
     
-    # print minimum
-    li $v0, 4
-    la $a0, max
-    syscall
+    blt $a0, $t3, novo_min	
+    j max_check
+    novo_min: move $t3, $a0		
+    j att_cont
+    
+    max_check:
+    bgt $a0, $t2, novo_max	
+    j att_cont
+    
+    novo_max: move $t2, $a0		
+    
+    att_cont:
+    addi $t1, $t1, 4    	
+    addi $t0, $t0, 1    	
+    b loop       		
+final:  
+        la $a0, max   	
+    	li $v0, 4       	               
+    	syscall
+    	
+	li $v0, 1		
+	move $a0, $t2		
+	syscall			
 
-    li $v0, 1
-    move $a0, $t2
-    syscall 
+	la $a0, min   	
+    	li $v0, 4       	              
+    	syscall
+	
+	li $v0, 1		
+	move $a0, $t3		
+	syscall			
+	
+	la $a0, resp   	
+    	li $v0, 4       	               
+    	syscall
 
-    # print maximum
-    li $v0, 4
-    la $a0, min
-    syscall
+	add $t4, $t2, $t3	
+	div $t5, $t4, 2		
+	li $v0, 1		
+	move $a0, $t5		
+	syscall
+    	jr $ra      		
 
-    li $v0, 1
-    move $a0, $t3
-    syscall 
-
-    # exit
-    li $v0, 10
-    syscall
 	
